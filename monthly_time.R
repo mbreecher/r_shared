5c81016aacdc34576ae01be3064389b0b79d00bd
mike breecher <michaelbreecher@gmail.com>
2015-01-02 15:42:12 -0700

    simplified monthly_time ddply operation for clarity and more uniform operation.

--- monthly_time.R
+++ monthly_time.R
@@ -47,43 +47,28 @@ timelog_with_status <- function(){
                                         type <- "Full Service"
                                       }else if(TRUE %in% (current_services %in% c("Maintenance"))){ #if not, see if they have a maintenance package
                                         type <- "Basic"
-                                      }# else, use default of DIY
-                                      if(TRUE %in% (current_forms %in% c("10-K", "K-K", "Q-K"))){ #see if current period form types are K projects
-                                        form <- c("K")
-                                      }else if(TRUE %in% (current_forms %in% c("10-Q", "Q-Q", "K-Q"))){ #if not, see if current period form types are Q projects
-                                        form <- c("Q")
-                                      }# else, use YED. see below
+                                      }
                                     }else{ #if no current services, see if the hours were logged to a future service
                                       if(TRUE %in% (x$Service.Type %in% full_service_types)){
                                         type <- "Full Service"
                                       }else if(TRUE %in% (x$Service.Type %in% c("Maintenance"))){
                                         type <- "Basic"
                                       }
-                                      if(TRUE %in% (x$Form.Type %in% c("10-K", "K-K", "Q-K"))){
-                                        form <- c("K")
-                                      }else if(TRUE %in% (x$Form.Type %in% c("10-Q", "Q-Q", "K-Q"))){
-                                        form <- c("Q")
-                                      }
                                     }
-                                    if(form %in% c("")){ #for DIYs, see if they have a YED assigned
-                                      if (length(unique(services[services$Account.Name %in% x$Account.Name & !(services$Year.End %in% c("     ")),]$Year.End)) > 0){
-                                            year_end <- as.Date(unique(services[services$Account.Name %in% x$Account.Name & !(services$Year.End %in% c("     ")),]$Year.End), format = "%m/%d")
-                                            form <- c("Q")
-                                             if(as.numeric(unique(x$Date) - year_end)%%365 >= 360 & as.numeric(unique(x$Date) - year_end)%%365 <= 95 & 
-                                                  !is.na(unique(x$Date)) & !is.na(year_end)){
-                                               form <- c("K")
-                                             }
-                                      }else if(dim(missing_yed[missing_yed$CIK %in% x$CIK,])[1] > 0){
-                                            year_end <- missing_yed[missing_yed$CIK %in% x$CIK,]$Year.End
-                                            form <- c("Q")
-                                            if(as.numeric(unique(x$Date) - year_end)%%365 >= 360 & as.numeric(unique(x$Date) - year_end)%%365 <= 95 & 
-                                                 !is.na(unique(x$Date)) & !is.na(year_end)){
-                                              form <- c("K")
-                                            }
-                                      }
+
+                                    if (length(unique(services[services$Account.Name %in% x$Account.Name & !(services$Year.End %in% c("     ")),]$Year.End)) > 0){
+                                          year_end <- as.Date(unique(services[services$Account.Name %in% x$Account.Name & !(services$Year.End %in% c("     ")),]$Year.End), format = "%m/%d")
+                                    }else if(dim(missing_yed[missing_yed$CIK %in% x$CIK,])[1] > 0){
+                                          year_end <- as.Date(missing_yed[missing_yed$CIK %in% x$CIK,]$Year.End, format = "%m/%d")
+                                    }
+                                    if((as.numeric(unique(x$Date) - year_end)%%365 >= 360 | as.numeric(unique(x$Date) - year_end)%%365 <= 95) & 
+                                         !is.na(unique(x$Date)) & !is.na(year_end)){
+                                      form <- c("K")
+                                    }else{
+                                      form <- c("Q")
                                     }
                                     
-                                    data.frame(xbrl_status = type, form_type = form)
+                                    data.frame(xbrl_status = type, form_type = form, year_end = as.Date(year_end), calc = as.numeric(unique(x$Date) - year_end)%%365)
                                 })
   proc.time() - ptm
   
