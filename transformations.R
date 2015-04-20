e831e4aa4821e05498b6342e0ad785c8747ab97e
mike breecher <michaelbreecher@gmail.com>
2015-04-20 16:12:30 -0700

    updated collapsed_opportunities function to update the dataframe with the account name in the services dataframe.

--- transformations.R
+++ transformations.R
@@ -4,6 +4,7 @@ collapsed_opportunities <- function(...){
   source("transformations.r")
   
   opps <- import_opportunities()
+  services <- import_services()
   collapsed_time <- collapsed_time_with_billable(include_incomplete = T)
   
   result <- merge(opps[!is.na(opps$Line.Item.18.Digit.Id) & !opps$Line.Item.18.Digit.Id %in% c(""),], 
@@ -21,6 +22,10 @@ collapsed_opportunities <- function(...){
       result[result$Services.ID %in% id,]$Sales.Price <- price_update[price_update$Services.ID %in% id,]$sales_price_updated 
     }
   }
+  
+  names(result)[names(result) %in% "Account.Name"] <- "Opportunity.Account.Name"
+  service_target <- services[,names(services) %in% c("Account.Name", "OpportunityLineItem.Id")]
+  result <- merge(result, service_target, by.x = "Line.Item.18.Digit.Id", by.y = "OpportunityLineItem.Id")
   
   result$monthyear <- format(result$filing.estimate, format = "%y-%m")
   
