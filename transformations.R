ee00a888ae8dfc74529747a8dc58f9735acff586
mike breecher <michaelbreecher@gmail.com>
2015-05-14 16:09:41 -0700

    DRY change to 42, and error correction to transformations

--- transformations.R
+++ transformations.R
@@ -6,8 +6,8 @@ collapsed_opportunities <- function(){
   opps <- import_opportunities()
   services <- import_services()
   # collapsed_time <- collapsed_time_with_billable(include_incomplete = T)
-  collapsed_time <- collapsed_time(complete = F, ...)
-  simple_collapsed <- collapsed_time[,!names(collapsed_time) %in% names(opps)]
+  collapsed_time_df <- collapsed_time(complete = F)
+  simple_collapsed <- collapsed_time_df[,!names(collapsed_time_df) %in% names(opps)]
   
   result <- merge(opps[!is.na(opps$Line.Item.18.Digit.Id) & !opps$Line.Item.18.Digit.Id %in% c(""),], 
                   simple_collapsed[!is.na(simple_collapsed$OpportunityLineItem.Id),], 
