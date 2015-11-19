514761fa6099c80a10d31ccc34974efb7ad5976e
mike breecher <michaelbreecher@gmail.com>
2015-11-19 09:40:59 -0700

    remove package dependency

--- headings.R
+++ headings.R
@@ -1,5 +1,4 @@
 weekyear_to_written <- function(old_names){
-require(lubridate)
   new_names <- c()
   all_dates <- seq(as.Date("2012/01/01"), Sys.Date() + days(30), by = "day")
   all_dates <- data.frame(date = as.Date(all_dates), weekyear = format(all_dates, format = "%y-%U"))
