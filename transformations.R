057dc1f4272e82894ef92a394502c76e1ce2733f
mike breecher <michaelbreecher@gmail.com>
2015-11-19 10:55:12 -0700

    added optional most_active determination to collapsed opportunities

--- transformations.R
+++ transformations.R
@@ -1,12 +1,15 @@
-collapsed_opportunities <- function(){
+collapsed_opportunities <- function(most_active = F){
   setwd("C:/R/workspace/shared")
   source("import_functions.r")
   source("transformations.r")
   
   opps <- import_opportunities()
   services <- import_services()
-  # collapsed_time <- collapsed_time_with_billable(include_incomplete = T)
-  collapsed_time_df <- collapsed_time_simple(complete = F)
+  if(most_active){
+    collapsed_time_df <- collapsed_time_with_most_active(complete = F)
+  }else{
+    collapsed_time_df <- collapsed_time_simple(complete = F)  
+  }
   simple_collapsed <- collapsed_time_df[,!names(collapsed_time_df) %in% names(opps)]
   
   result <- merge(opps[!is.na(opps$Line.Item.18.Digit.Id) & !opps$Line.Item.18.Digit.Id %in% c(""),], 
