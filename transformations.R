b9d1d70fa89d2949b430ed5eab8a9a415b6f509b
mike breecher <michaelbreecher@gmail.com>
2015-11-19 09:17:25 -0700

    updated collapsed time function in collapsed opps

--- transformations.R
+++ transformations.R
@@ -6,7 +6,7 @@ collapsed_opportunities <- function(){
   opps <- import_opportunities()
   services <- import_services()
   # collapsed_time <- collapsed_time_with_billable(include_incomplete = T)
-  collapsed_time_df <- collapsed_time(complete = F)
+  collapsed_time_df <- collapsed_time_simple(complete = F)
   simple_collapsed <- collapsed_time_df[,!names(collapsed_time_df) %in% names(opps)]
   
   result <- merge(opps[!is.na(opps$Line.Item.18.Digit.Id) & !opps$Line.Item.18.Digit.Id %in% c(""),], 
