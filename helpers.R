44d867c26392068c8c1eb5bbef357c26717f8703
mike breecher <michaelbreecher@gmail.com>
2015-12-11 12:17:45 -0700

    auto-update mechanism to populate ps start dates for newly detected PS Staff

--- helpers.R
+++ helpers.R
@@ -11,6 +11,8 @@ is_psm <- function(user_vector = NULL, date_vector = NULL, title_vector = NULL){
   setwd('C:/R/workspace/source')
   start_dates <- readRDS("ps_start_dates.Rda")
   
+  # add 
+  
   result <- data.frame(User = user_vector, Date = date_vector, User.Title = title_vector, is_psm = 0)
 
   ## result[result$User %in% start_dates[is.na(start_dates$Start.Date) & is.na(start_dates$End.Date), ]$Full.Name, ]$is_psm <- 1 #psms who are still in PS
@@ -36,10 +38,30 @@ is_psm <- function(user_vector = NULL, date_vector = NULL, title_vector = NULL){
   #case 2: unknown PSMs
   ps_titles <- c("Professional Services Managers", "PSM", "Senior Team Manager, Professional Services", "PSM Team Manager")
   if(length(result[result$User.Title %in% ps_titles & result$is_psm %in% "0", ]$is_psm) > 0){
-    print("no start date information found for:")
-    print(as.character(unique(result[result$User.Title %in% ps_titles & result$is_psm %in% "0", ]$User)))
     result[result$User.Title %in% ps_titles, ]$is_psm <- 1
   }
+  
+  #add unknown PSMs to start dates dataframe
+  if(length(unique(result[result$User.Title %in% ps_titles & !result$User %in% start_dates$Full.Name, ]$User)) > 0){
+    print("new psms detected. Importing Timelog and updating ps start dates...")
+    setwd("C:/R/workspace/shared")
+    source("import_functions.r")
+    
+    for(name in unique(result[result$User.Title %in% ps_titles & !result$User %in% start_dates$Full.Name, ]$User)){
+      print(paste("name added to start dates: ", name), sep = "")
+      if(dim(timelog[timelog$User %in% name,])[1] > 0){
+        start_dates <- rbind(start_dates, 
+                                  data.frame(Full.Name = name, 
+                                             Start.Date = min(timelog[timelog$User %in% name & !is.na(timelog$Hours),]$Date), 
+                                             End.Date = NA))  
+      }else{
+        print("no service time logged")
+      }
+    }
+    setwd("c:/r/workspace/source")
+    saveRDS(start_dates, file = "ps_start_dates.Rda")
+  }
+  
   
   result$is_psm
 }
